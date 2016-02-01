63519922d48f0c742ccb512288f97491a1b8299d
Eric Johnson <ejohnson@showpitch.com>
2016-02-01 12:19:19 -0700

    Updated output names

--- src/index.js
+++ src/index.js
@@ -1,49 +1,42 @@
 /**
  * Created by ericjohnson on 11/19/15.
  */
-
-
 import {EventAggregator} from 'aurelia-event-aggregator';
-import {Storage} from 'aurelia-storage';
+import {Storage} from 'storage';
+export class PostBox {
+  constructor() {
+    this.eventAggregator = new EventAggregator();
+    this.storage = new Storage();
+  }
 
-export class PostBox{
-    constructor(){
-        this.eventAggregator = new EventAggregator();
-        this.storage = new Storage();
+  subscribe(topic, callback, getLatestValue = false) {
+
+    // if 'getLatestValue' call callback with latest value
+    if (getLatestValue) {
+      callback(this.storage.retrieve(topic));
     }
+    // subscribe to topic and return unsubscribe option
+    return this.eventAggregator.subscribe(topic, payload => {
+      callback(payload);
+    })
+  }
 
-    subscribe(topic, callback, getLatestValue = false) {
-
-        // if 'getLatestValue' call callback with latest value
-        if (getLatestValue) {
-            callback(this.storage.retrieve(topic));
-        }
-
-        // subscribe to topic and return unsubscribe option
-        return this.eventAggregator.subscribe(topic, payload => {
-            callback(payload);
-        })
+  publish(topic, value, local = false) {
+    let skipStorage = topic.startsWith('temp'),
+      isLocal = local || topic.startsWith('local');
+    if (!skipStorage) {
+      // store latest value in library
+      this.storage.store(topic, value, undefined, !isLocal);
     }
+    // publish topic
+    return this.eventAggregator.publish(topic, value);
+  }
 
-    publish(topic, value, local = false) {
-        let skipStorage = topic.startsWith('temp'),
-            isLocal = local || topic.startsWith('local');
+  clear(topic) {
 
-        if(!skipStorage) {
-            // store latest value in library
-            this.storage.store(topic, value, undefined, !isLocal);
-        }
-
-        // publish topic
-        return this.eventAggregator.publish(topic, value);
-    }
-
-    clear(topic){
-
-        // publish the empty value
-        this.eventAggregator.publish(topic, undefined);
-
-        // delete topic from library
-        this.storage.remove(topic);
-    }
+    // publish the empty value
+    this.eventAggregator.publish(topic, undefined);
+    // delete topic from library
+    this.storage.remove(topic);
+  }
 }
